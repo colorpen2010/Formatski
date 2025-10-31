2617647a906bf433bad168a46f752b64cd28cb9c
GmodHunter <sdsad@gmail.com>
2025-10-31 14:10:30 +0200

    ver 5

--- City_simulator.py
+++ City_simulator.py
@@ -1,17 +1,29 @@
 import random
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton,QGraphicsScene, QWidget
-from PySide6.QtGui import QPen, QStandardItemModel
+from PySide6.QtGui import QPen, QStandardItemModel,QStandardItem
 from PySide6.QtCore import  QUrl,QStringListModel,Qt,QLineF,QRect, QPoint
 from forms_2 import Ui_MainWindow
 from ui_building_menu import  Ui_Form
 
-z=QStandardItemModel(2,3)
+z=QStandardItemModel(0,3)
 s=Ui_MainWindow()
 t=Ui_Form()
 e=QApplication()
 r=QGraphicsScene()
 a=QLineF()
 w=QPen()
+item=QStandardItem()
+
+def row_adder(name,x,y):
+    row = [
+        QStandardItem(str(name)),
+        QStandardItem(str(x)),
+        QStandardItem(str(y))
+    ]
+    z.appendRow(row)
+
+row_adder('house',3,4)
+row_adder("shop",3,3)
 
 rect_size=50,50
 
@@ -30,6 +42,7 @@ for b in range(int(-550),151,rect_size[0]):
 # d.setRect(-0,50,50,50)
 # d.setRect(50,100,50,50)
 
+
 def building_menu():
     cifri=q.geometry()
     print(cifri)
@@ -37,6 +50,16 @@ def building_menu():
     f.move(cifri.right()+5,cifri.top()-30)
     f.show()
 
+
+def on_row_selected(current):
+    row = current.row()
+    name = z.item(row, 0)
+    x = z.item(row, 1)
+    y = z.item(row, 2)
+    print(name.text())
+    t.lineEdit.setText(name.text())
+    t.spinBox.setValue(int(x.text()))
+    t.spinBox_2.setValue(int(y.text()))
 
 
 w.setColor("red")
@@ -47,11 +70,15 @@ s.Building_menu.triggered.connect(building_menu)
 
 z.setHorizontalHeaderLabels(["name","x","y"])
 t.tableView.setEditTriggers(t.tableView.EditTrigger.NoEditTriggers)
+
 t.tableView.setModel(z)
+
+t.tableView.selectionModel().currentChanged.connect(on_row_selected)
 
 s.graphicsView.setScene(r)
 for b in rects:
  r.addRect(b)
+
 
 q.setWindowTitle("Skylines Cities")
 
